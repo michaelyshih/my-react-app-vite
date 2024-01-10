4080ed56e511c036b6d88abe12748bd36ac99589
shihyulung <shihyulung@yahoo.com.tw>
2024-01-09 20:47:00 -0800

    finish 01-useState

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,34 @@
+import { isObjectLiteralElementLike, isOptionalChain } from 'typescript';
+import {data, people} from '../../../data'
+import { useState } from 'react';
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data)
+
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) =>  person.id !== id )
+    setPeople(newPeople)
+   }
+
+  const clearAllitems = () => {
+    setPeople([])
+   }
+
+   return (
+    <div>
+      {people.map(({id,name}) => {
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type='button' onClick={() => removeItem(id) }>remove</button>
+          </div>
+        )
+       })}
+       <button type='button' className='btn' style={{marginTop: '2rem'}} onClick={clearAllitems}>
+        clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,32 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [person, setPerson]= useState({
+    name: 'Peter',
+    age: 24,
+    hobby: 'Read Books'
+  })
+
+  const showPerson = () => {
+    const newPerson = {
+      ...person,
+      name: 'John',
+      age: 28,
+      hobby: 'Scream at The Computer'
+    }
+    setPerson(newPerson)
+   }
+
+  return (
+    <div>
+      <h2>useState object example</h2>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Engjoys To: {person.hobby}</h4>
+      <button className='btn' type='button' onClick={showPerson}>Show John</button>
+    </div>
+  );
 };
 
 export default UseStateObject;
